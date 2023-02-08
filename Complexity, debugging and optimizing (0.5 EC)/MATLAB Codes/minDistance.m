893827f4bc36e34efda27720713d36ea8034966a
Kirtan Patel <59549172+kirtan2605@users.noreply.github.com>
2023-02-08 21:58:38 +0530

    Delete Complexity, debugging and optimizing (0.5 EC) directory

--- Complexity, debugging and optimizing (0.5 EC)/MATLAB Codes/minDistance.m
+++
@@ -1,9 +0,0 @@
-function d = minDistance(x,y,z)
-% Given a set of 3D points specified by column vectors x,y,z, this
-% function computes the minimum distance to the origin
-% compute distance for every point
-
-d = sqrt(x.^2 + y.^2 + z.^2);
-
-% get minimum distance
-d = min(d);
